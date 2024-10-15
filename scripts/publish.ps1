58f9f1cb3a86e9c19173be6b93f21fbf37cc4dd9
the-poodle-doodler <poodle-doodler@outlook.com>
2024-10-15 23:45:10 +0300

    fix publisher script

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,8 +1,7 @@
-$ErrorActionPreference = "Stop"
 $OUTPUT_DIR = "docs"
 $BROWSER_DIR = "docs/browser"
 
-function Check-Error {
+function Test-Error {
   if ($LASTEXITCODE -ne 0) {
       Write-Output "Error detected. Exiting script."
       exit $LASTEXITCODE
@@ -17,10 +16,10 @@ $currentHash = git rev-parse HEAD
 $shortHash = $currentHash.Substring(0, 8)
 
 git checkout release
-Check-Error
+Test-Error
 
 git merge main
-Check-Error
+Test-Error
 
 Remove-Item -Path $OUTPUT_DIR -Recurse -Force
 
@@ -28,7 +27,7 @@ ng build --output-path $OUTPUT_DIR --base-href /
 
 Get-ChildItem -Path $BROWSER_DIR -Recurse | ForEach-Object {
     $destPath = Join-Path -Path $OUTPUT_DIR -ChildPath $_.FullName.Substring($BROWSER_DIR.Length)
-    Move-Item -Path $_.FullName -Destination $OUTPUT_DIR
+    Move-Item -Path $_.FullName -Destination $destPath
 }
 Remove-Item -Path $BROWSER_DIR -Recurse -Force
 Remove-Item -Path "docs/3rdpartylicenses.txt" -Force
