172f3fb8f0a1b7c97aa824fa274a1a8d6899498d
the-poodle-doodler <poodle-doodler@outlook.com>
2024-10-17 23:12:52 +0300

    add logo a background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from "./logo/logo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, RouterOutlet],
+  imports: [FooterComponent, LogoComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
