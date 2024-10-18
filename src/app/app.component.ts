d6523170b80a3d5e1f4a1cc4f7f4d01732cb46b8
the-poodle-doodler <poodle-doodler@outlook.com>
2024-10-18 22:31:27 +0300

    ready steady go -- next step add content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { LogoComponent } from "./logo/logo.component";
 import {StickerComponent} from "./sticker/sticker.component";
 import {NgForOf} from "@angular/common";
 import {GalleryItem} from "./common/galleryItem.model";
+import {NgxPaginationModule} from "ngx-pagination";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, LogoComponent, RouterOutlet, StickerComponent, NgForOf],
+  imports: [FooterComponent, LogoComponent, RouterOutlet, StickerComponent, NgForOf, NgxPaginationModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   images: GalleryItem[] =[
     new GalleryItem('first-poodle', 'The first poodle doodle', 'Created for the #penstoretalents2024 competition', 1.31, 10, 17, 6),
     new GalleryItem('first-poodle', 'The first poodle doodle', 'Created for the #penstoretalents2024 competition', 1.31, 10, 17, 6),
@@ -22,20 +23,5 @@ export class AppComponent implements OnInit {
     new GalleryItem('first-poodle', 'The first poodle doodle', 'Created for the #penstoretalents2024 competition', 1.31, 10, 17, 6),
     new GalleryItem('first-poodle', 'The first poodle doodle', 'Created for the #<a href="https://penstore.fi/fi/talents" target="_blank">penstoretalents2024</a> competition', 1.31, 10, 17, 6)
   ];
-  public screenWidth: any;
-
-  ngOnInit() {
-    this.screenWidth = window.innerWidth;
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(_event: any) {
-    this.screenWidth = window.innerWidth;
-  }
-
-  columnRange(): number[] {
-    let numberOfColumns = this.screenWidth / 350;
-    console.log(numberOfColumns);
-    return [...Array(Math.floor(numberOfColumns)).keys()];
-  }
+  currentPage = 0;
 }
